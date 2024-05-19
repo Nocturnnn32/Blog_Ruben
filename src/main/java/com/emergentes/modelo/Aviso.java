883a3e2ba579a00d3d263fc5b2f8c46d8df4eb9a
Ruben Condori Cosme <162364859+Nocturnnn32@users.noreply.github.com>
2024-05-18 22:07:01 -0400

    Version 1.0

---
+++ src/main/java/com/emergentes/modelo/Aviso.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.emergentes.modelo;
+
+/**
+ *
+ * @author RUBEN
+ */
+public class Aviso {
+    private int id;
+    private String fecha;
+    private String titulo;
+    private String contenido;
+
+    public Aviso() {
+        this.id=0;
+        this.fecha="";
+        this.titulo="";
+        this.contenido="";
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+    
+    
+    
+    
+}
